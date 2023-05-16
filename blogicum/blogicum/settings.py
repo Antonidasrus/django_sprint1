99a4311705ead78ff0a7feded594705100481249
Коновалов Антон Романович <akonovalov@stan-group.com>
2023-05-16 12:47:39 +0300

    4th try

--- blogicum/blogicum/settings.py
+++ blogicum/blogicum/settings.py
@@ -10,19 +10,25 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
+import environ
+
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+env = environ.Env(DEBUG=(bool, False))
+
+BASE_DIR = Path(__file__).resolve().parent.parent
+
+environ.Env.read_env(BASE_DIR / '.env')
+
+SECRET_KEY = env.str('SECRET_KEY', 'secret')
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 ALLOWED_HOSTS = ['*']
-
-# Default SECRET_KEY for tests
-SECRET_KEY = 'secret'
 
 # Application definition
 
